1431b02793be7957b200a1c6dcec6c52da047f3b
waitblock <65051086+waitblock@users.noreply.github.com>
2021-08-09 17:45:01 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import discord
+import os
+import requests
+from discord.ext import commands
+
+bot = commands.Bot(("l!", "link ", "l?", "link?", "link!"), case_insensitive=True, help_command=None)
+
+
+@bot.command(name="check", aliases=['check_link', 'checklink', 'linkcheck', 'link_check', 'trace', 'trace_link', 'tracelink'])
+async def check(ctx, *, link):
+    try:
+        r = requests.head(link, allow_redirects=True)
+        result = "Link Trace Results:\n"
+        print(r.url)
+        for hist in r.history:
+            result += ("- <" + hist.url + ">\n")
+            print(hist.url)
+        result += ("- <" + r.url + ">\n")
+        await ctx.send(result)
+    except:
+        ...
+
+
+@bot.command(name="help", aliases=["bothelp"])
+async def bot_help(ctx):
+    with open(os.path.join("docs", "help.txt"), "r") as f:
+        await ctx.send(f.read())
+
+
+@bot.command(name="version", aliases=["botversion"])
+async def bot_version(ctx):
+    with open(os.path.join("docs", "version.txt"), "r") as f:
+        await ctx.send(f.read())
+
+
+@bot.event
+async def on_connect():
+    print("Bot connected to Discord API")
+    await bot.change_presence(activity=discord.Game(name="For help type link!help"))
+
+bot.run(os.getenv("token"))
