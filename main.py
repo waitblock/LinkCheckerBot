ba849f9a9e9510f0041c4fea32cd2b3f7786376f
waitblock <65051086+waitblock@users.noreply.github.com>
2021-08-09 22:43:52 -0400

    add ping and add error statements for the tracelink command

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 import os
 import requests
 from discord.ext import commands
+import traceback
 
 bot = commands.Bot(("l!", "link ", "l?", "link?", "link!"), case_insensitive=True, help_command=None)
 
@@ -17,14 +18,23 @@ async def check(ctx, *, link):
             index += 1
         result += (f"Destination ({r}) - <" + r.url + ">\n")
         await ctx.send(result)
+    except requests.exceptions.MissingSchema:
+        await ctx.send("Try adding http:// to the front of the link.")
+    except requests.exceptions.ConnectionError:
+        await ctx.send("The URL refused to connect.")
     except:
-        ...
+        await ctx.send("An error occurred.")
 
 
 @bot.command(name="help", aliases=["bothelp"])
 async def bot_help(ctx):
     with open(os.path.join("docs", "help.txt"), "r") as f:
         await ctx.send(f.read())
+
+
+@bot.command(name="ping", aliases=["botping"])
+async def ping(ctx):
+    await ctx.send(f"Pong! {round(bot.latency*100, 2)}ms")
 
 
 @bot.command(name="version", aliases=["botversion"])
